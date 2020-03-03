31c8022c4968f9a8022a5e5f3461b59b581c2d91
Lily Zhong <lilyzhongy@gmail.com>
2020-03-02 20:45:22 -0500

    Fix spacing

--- Divisibility.java
+++ Divisibility.java
@@ -5,6 +5,7 @@ public class Divisibility {
         Scanner stdin = new Scanner(System.in);
         System.out.print("Enter an integer: ");
         int num = stdin.nextInt();
+        
         // assigning to a variable will make your code more readable
         // be sure to give your variables meaningful names so that you can quickly understand when you look back at your code!
         boolean divByFive = (num % 5 == 0);
@@ -12,10 +13,13 @@ public class Divisibility {
         boolean divByEitherOr = (divByFive || divBySix);
         boolean divByBoth = (divByFive && divBySix);
         boolean divByOnlyOne = (divByFive && !divBySix) || (!divByFive && divBySix); // divided by one of them but not the other
+        
         // same as System.out.println("Is " + num + " divisible by 5 and 6? " + divByBoth);
         System.out.printf("Is %d divisible by 5 and 6? %b\n", num, divByBoth); 
+        
         // same as System.out.println("Is " + num + " divisible by 5 or 6? " + divByEitherOr);
         System.out.printf("Is %d divisible by 5 or 6? %b\n", num, divByEitherOr); 
+        
         // same as System.out.println("Is " + num + " divisible by 5 and 6, but not both? " + divByOnlyOne);
         System.out.printf("Is %d divisible by 5 or 6, but not both? %b\n", num, divByOnlyOne); 
     }
